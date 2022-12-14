b3f56a873e51b7421834337c8b30671dff9edb18
Khoa Nguyễn <khoaruoi69@gmail.com>
2022-12-14 16:24:38 +0700

    first commit

---
+++ API_Task/Models/Task.cs
@@ -0,0 +1,13 @@
+﻿namespace API_Task.Models
+{
+    public class Task
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public int? Status { get; set; }
+        public bool IsCompleted { get; set; }
+
+
+    }
+}
---
+++ API_Task/Models/Todo.cs
@@ -0,0 +1,6 @@
+﻿namespace API_Task.Models
+{
+    public class Todo
+    {
+    }
+}
